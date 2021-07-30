36a8823c67314764e8f01c4af768cd56045c77a4
RekaKR <RekaKR@users.noreply.github.com>
2021-07-30 14:44:21 +0200

    Update

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-//const http = require('http')
 const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer({})
 
@@ -9,7 +8,7 @@ proxy.on('proxyReq', (proxyReq, req, res, options) => {
   proxyReq.setHeader('X-USER-ID', '878465')
 })
 
-router.all('/*', (req, res, next) => {
+router.all('/*', (req, res) => {
   proxy.web(req, res, { target: 'http://localhost:3001/api/userboards' })
 })
 
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
+const jwt = require('jsonwebtoken')
 
+
+require('dotenv').config()
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_SECRET = process.env.GOOGLE_SECRET
+const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI
 
 router.get('/', (req, res) => {
+  fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code: req.query.code,
+      client_id: GOOGLE_CLIENT_ID,
+      client_secret: GOOGLE_SECRET,
+      redirect_uri: GOOGLE_REDIRECT_URI,
+      grant_type: "authorization_code"
+    }),
+  })
+    .then(res => res.json())
+    .then(data => console.log(jwt.decode(data.id_token)))
+    .catch(() => res.status(404).json({ message: 'Authentication failed!' }))
+
   res.json({ message: "Hello world" })
 })
 
